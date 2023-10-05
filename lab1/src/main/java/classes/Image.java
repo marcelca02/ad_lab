0f12a9f42a5f72513c3784535590a87e5ed9b4b0
marcelca02 <marcel.calleja@estudiantat.upc.edu>
2023-10-05 13:28:37 +0200

    image class

---
+++ lab1/src/main/java/classes/Image.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author marcel
+ */
+public class Image {
+    public int id; 
+    public String title;
+    public String description;
+    public String[] keywords;
+    public String author;
+    public String creator;
+    public String captureDate;
+    public String storageDate;
+    public String filename;
+    
+}
