d6337cd9668d8ccb8265eca49d134b22ec96b1a3
marcelca02 <marcel.calleja@estudiantat.upc.edu>
2023-11-16 12:29:02 +0100

    init lab4

---
+++ lab4/Client/src/main/java/Servlets/registroImagen.java
@@ -0,0 +1,172 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.constants;
+
+/**
+ *
+ * @author Max Pasten
+ */
+@MultipartConfig
+@WebServlet(name = "registroImagen", urlPatterns = {"/registroImagen"})
+public class registroImagen extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    private static final long serialVersionUID = 1L;
+       
+    public registroImagen() {
+        super();
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // Get registro_imagen form parameters
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+        String keywords = request.getParameter("keywords");
+        String author = request.getParameter("author");
+        String creator = request.getParameter("creator");
+        String capture_date = request.getParameter("capture_date");
+        String storage_date = request.getParameter("storage_date");
+        Part file=request.getPart("filename");
+        // Nombre de Archivo
+        String filename=file.getSubmittedFileName();
+        
+        
+        response.setContentType("text/html;charset=UTF-8");
+        
+        URL url = new URL("http://localhost:8080/RestAD/resources/jakartaee9/register");
+        // Conectar URL
+        try {
+            URLConnection myURLConnection = url.openConnection();
+            myURLConnection.connect();
+        }
+        catch (MalformedURLException e) {
+            // Fallo de URL
+            Logger.getAnonymousLogger().log(Level.SEVERE,"URL error", e);
+        } 
+        catch (IOException e){
+            // fallo de la conexion
+            Logger.getAnonymousLogger( ).log(Level.SEVERE, "I0 error",e);
+        }
+        
+        //Abrir una conexión HTTP
+        HttpURLConnection connection =(HttpURLConnection)url.openConnection();
+        // Configurar el método de la petición 
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+        // Escribir los parámetros
+        try (OutputStream output = connection.getOutputStream()){
+            // Construye la cadena de datos 
+            String data = "title=" + title +
+                            "&description=" +description +
+                            "&keywords=" +keywords +
+                            "&author=" +author +
+                            "&creator=" + creator+
+                            "&capture=" + capture_date +
+                            "&filename=" + filename;
+            
+            output.write(data.getBytes("UTF-8"));
+            output.close();
+            
+            System.out.println("ENVIADO REGISTRO");
+            // Recibe la respuesta del servidor
+            int responsecode = connection.getResponseCode();
+            if (responsecode == HttpURLConnection.HTTP_OK){
+                // La conexión fue exitosa
+                //response.getWriter( ).write("Datos enviados correctamente al servidor.");
+                
+                //String uploadPath="C:\\Users\\Max Pasten\\lab1\\images\\"+imageFileName;
+                String uploadPath=constants.IMAGESDIR+filename;
+                FileOutputStream fos=new FileOutputStream(uploadPath);
+                InputStream is=file.getInputStream();
+
+                byte[] dataImg=new byte[is.available()];
+                is.read(dataImg);
+                fos.write(dataImg);
+                fos.close();
+                
+                
+                // Redirect
+                response.sendRedirect("/Client/menu.jsp");
+            } else {
+                //response.getWriter().write("Error al enviar datos al servidor. Código de respuesta: " + responsecode);
+                response.sendRedirect("/Client/error.jsp");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            //response.getWriter().write("Error:"+ e.getMessage());
+            response.sendRedirect("/Client/error.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ lab4/Client/src/main/webapp/error.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : error
+    Created on : 30 sept 2023, 19:18:39
+    Author     : marcel
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login</title>        
+        <link rel="stylesheet" type="text/css" href="css/general.css"/>
+    </head>
+    <body>
+        <div class="navbar">
+            <h1>Error</h1>
+        </div>
+        <h1>¡Ha ocurrido un error!</h1>
+      <%
+      String referer = request.getHeader("Referer");
+      if (referer != null && !referer.isEmpty()){
+      %>
+        <a href="<% out.write(referer); %>">Volver atrás</a>
+      <%
+      }else {
+      %>
+        <a href="/">Ir a la página de inicio</a>
+      <%
+      }
+      %>
+    </body>
+</html>
