cbe2e327432fd8b700deab5d5f793af2397ed5c8
marcelca02 <marcel.calleja@estudiantat.upc.edu>
2023-12-13 21:47:37 +0100

    fix nombre db

--- exploratoria/app/__init__.py
+++ exploratoria/app/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def init_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.pr2'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pr2.db'
     db.init_app(app)
 
     with app.app_context():
