46eb73ffbbd3ac83800d6b58d45ab3abb9d8cb74
marcelca02 <marcel.calleja@estudiantat.upc.edu>
2023-12-28 23:52:51 +0100

    descarga funciona

--- exploratoria/Client_API_Python/src/main/java/utils/constants.java
+++ exploratoria/Client_API_Python/src/main/java/utils/constants.java
@@ -12,6 +12,7 @@ public class constants {
 
     public static final String IMAGESDIR = "/var/webapp/lab1/images/";
     public static final String APPDIR = "/var/webapp/lab1";   
+    public static final String DOWNLOADDIR = "/var/webapp/lab1/images/download/";
     //public static final String IMAGESDIR = "C:/Users/Max Pasten/lab1/client/";
     //public static final String APPDIR = "C:/Users/Max Pasten/lab1";
 
